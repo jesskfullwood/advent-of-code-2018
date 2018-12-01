0dac4695b15b69823b8e8d95bbe29d884f6c0bfa
jesskfulwood <jesskfulwood@gmail.com>
2018-12-01 12:05:17 +0000

    q1

---
+++ app/q01.hs
@@ -0,0 +1,43 @@
+module Main where
+
+import           Data.Maybe
+import           Debug.Trace
+import           Text.Read
+import Data.Set
+
+
+textToInts :: String -> Maybe [Int]
+textToInts text =
+  sequence $ signedNumToInt <$> (lines text)
+
+signedNumToInt :: String -> Maybe Int
+signedNumToInt num =
+  case num of
+    '+' : rest -> readMaybe rest
+    '-' : rest -> (*) (-1) <$> readMaybe rest
+    _          -> Nothing
+
+
+seenBefore :: Set Int -> Int -> Either Int (Set Int)
+seenBefore set freq =
+  case lookupIndex freq set of
+    Nothing -> Right $ insert freq set
+    Just _ -> Left freq
+
+findRepeatedFreq :: [Int] -> Int
+findRepeatedFreq freqs =
+  let checkFreq (freq:rest) set cum =
+        let cumFreq = freq + cum in
+        case seenBefore set cumFreq of
+          Left res -> res
+          Right set' -> checkFreq rest set' cumFreq
+  in
+    checkFreq (cycle freqs) empty 0
+
+main :: IO ()
+main =
+  do
+    text <- readFile "data/01-1"
+    let vals = fromMaybe [] $ textToInts text
+    putStrLn .  (++) "V1: " $ show $ sum vals
+    putStrLn .  (++) "V2: " $ show $ findRepeatedFreq vals
