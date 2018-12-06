13e609063db4339af17ac3040c3ccd7410818203
jesskfulwood <jesskfulwood@gmail.com>
2018-12-06 11:31:44 +0000

    q2.2

--- app/q02.hs
+++ app/q02.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
-import Data.List
-import Debug.Trace
+import           Data.List
+import           Debug.Trace
 
 
 ingest :: IO [String]
@@ -30,15 +32,26 @@ countThrees :: String -> Bool
 countThrees word = runCount word 3
 
 boolInt :: Bool -> Int
-boolInt True = 1
+boolInt True  = 1
 boolInt False = 0
+
+
+countDiffs :: String -> String -> Int
+countDiffs word1 word2 = foldl (\acc (l, r) -> acc + boolInt (l /= r)) 0 $ zip word1 word2
+
+filterDiffs :: String -> String -> String
+filterDiffs word1 word2 = fmap fst $ filter (uncurry (==)) $ zip word1 word2
 
 main :: IO ()
 main = do
-  strs <- ingest
-  let sortedStrs = sort <$> strs
+  wordList <- ingest
+  let sortedStrs = sort <$> wordList
   let twoCt = foldl (\ct inp -> ct + boolInt (countTwos inp)) 0 sortedStrs
   let threeCt = foldl (\ct inp -> ct + boolInt (countThrees inp)) 0 sortedStrs
   putStrLn $ "Twos: " ++ show twoCt
   putStrLn $ "Threes: " ++ show threeCt
   putStrLn $ show $ twoCt * threeCt
+  let pairs :: [(String, String)] = wordList >>= (\left -> (wordList >>= \right -> pure (left, right)))
+  let unqs = filter (uncurry (>)) pairs
+  let differByOne = filter (\(l, r) -> (countDiffs l r) == 1) unqs
+  putStrLn $ uncurry filterDiffs $ head differByOne
