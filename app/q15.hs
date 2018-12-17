c14dd04f88072fa7490c26421704767b4bf0b994
jesskfulwood <jesskfulwood@gmail.com>
2018-12-17 15:02:45 +0000

    q15 wip

--- app/q15.hs
+++ app/q15.hs
@@ -3,7 +3,8 @@
 module Main where
 
 import           Data.List.Split             (chunksOf)
-import qualified Data.Map.Strict             as S
+import qualified Data.Map.Strict             as Map
+import           Data.Maybe                  (isNothing, listToMaybe, mapMaybe)
 import           Data.Sequence               (Seq (..), empty, (|>))
 import           Data.Vector.Unboxed         ((!))
 import qualified Data.Vector.Unboxed         as V
@@ -12,11 +13,16 @@ import qualified Debug.Trace                 as T
 import           Util
 
 
-data Entity = Elf | Goblin deriving Show
+data Creature = Elf | Goblin deriving (Show, Eq)
+type Entity = (Creature, Coord, Int)
+
+entityToChar :: Entity -> Char
+entityToChar (Elf, _, _)    = 'E'
+entityToChar (Goblin, _, _) = 'G'
 
 type Grid = (V.Vector Bool, Int, Int)
 type Coord = (Int, Int)
-type Entities = S.Map Coord (Entity, Int)
+type Entities = Map.Map Coord Entity
 type Bfs = M.IOVector Int
 
 initHealth :: Int
@@ -31,11 +37,11 @@ parseFile str =
                                       case char of
                                           '#' -> (False:grid', entities'')
                                           '.' -> (True:grid', entities'')
-                                          'E' -> (True:grid', S.insert coord (Elf, initHealth) entities'')
-                                          'G' -> (True:grid', S.insert coord (Goblin, initHealth) entities'')
+                                          'E' -> (True:grid', Map.insert coord (Elf, coord, initHealth) entities'')
+                                          'G' -> (True:grid', Map.insert coord (Goblin, coord, initHealth) entities'')
                                           _  -> error ("Bad char" ++ [char])
                                    ) (grid, entities') row
-                    ) ([], S.empty) rows
+                    ) ([], Map.empty) rows
       nrows = length rows
       ncols = length (rows !! 0)
   in
@@ -56,8 +62,8 @@ right (x, y) = (x + 1, y)
 left :: Coord -> Coord
 left (x, y) = (x - 1, y)
 
-get :: Grid -> Coord -> Bool
-get (grid, xs, _) (x, y) =
+isValidSquare :: Grid -> Coord -> Bool
+isValidSquare (grid, xs, _) (x, y) =
   grid ! (xs * y + x)
 
 readBfs :: Bfs -> Int -> Coord -> IO (Maybe Int)
@@ -83,36 +89,63 @@ intToChar i =
   else
     '+'
 
-showGrid :: Grid -> String
-showGrid (grid, xs, _) =
-  unlines . (chunksOf xs) $ boolToChar <$> V.toList grid
+update :: [a] -> Int -> a -> [a]
+update list posn val =
+  take posn list ++ val:(drop (posn + 1) list)
+
+showGrid :: Grid -> Entities -> String
+showGrid (grid, xs, _) entities =
+  let chars = boolToChar <$> V.toList grid
+      chars'' = foldl (\chars' ((x, y), entity) ->
+                        update chars' (y * xs + x) (entityToChar entity)) chars (Map.toList entities)
+  in
+  unlines (chunksOf xs chars'')
 
 
 showDijkstra :: V.Vector Int -> Int -> String
 showDijkstra bfs xs =
   unlines . (chunksOf xs) $ intToChar <$> V.toList bfs
 
+sourceInt :: Int
 sourceInt = 9999
 
-dijkstra :: Grid -> Coord -> IO (V.Vector Int)
-dijkstra grid@(grid', _, _) source = do
+entityTurn :: Grid -> Entities -> Entity -> IO Entities
+entityTurn grid entities entity =
+  case willAttack entity entities of
+    -- do the attack
+    Just (_, coord, _) -> pure $ Map.update (\(c, _, hp) -> if hp > 3 then Just (c, coord, hp - 3) else Nothing) coord entities
+    Nothing -> entityMove grid entities entity
+
+willAttack :: Entity -> Entities -> Maybe Entity
+willAttack (c, coord, _) entities =
+  let lookup' f = Map.lookup (f coord) entities >>= (\e2@(c2, _, _) -> if c /= c2 then Just e2 else Nothing)
+  in listToMaybe . (mapMaybe lookup') $ [up, right, down, left]  -- attack order
+
+entityMove :: Grid -> Entities -> Entity -> IO Entities
+entityMove grid entities (_, coord, _) = do
+  dists <- dijkstra grid entities coord
+  undefined
+
+
+dijkstra :: Grid -> Entities -> Coord -> IO (V.Vector Int)
+dijkstra grid@(grid', _, _) entities source = do
   let
     -- set initial positions
     posns = empty |> (source, sourceInt) |> (up source, 1) |> (down source, 1) |> (left source, 1) |> (right source, 1)
     bfs = M.new (V.length grid')
-  bfs' <- dijkstra' grid source bfs posns
+  bfs' <- dijkstra' grid entities source bfs posns
   V.freeze bfs'
 
-dijkstra' :: Grid -> Coord -> IO Bfs -> Seq (Coord, Int) -> IO Bfs
-dijkstra' _ _ bfs Empty = bfs  -- No more positions to visit
-dijkstra' grid@(_, xs, _) source bfs ((nextPosn,dist):<|posns) =
-  if (T.traceShow nextPosn) get grid nextPosn then
+dijkstra' :: Grid -> Entities -> Coord -> IO Bfs -> Seq (Coord, Int) -> IO Bfs
+dijkstra' _ _ _ bfs Empty = bfs  -- No more positions to visit
+dijkstra' grid@(_, xs, _) entities source bfs ((nextPosn,dist):<|posns) =
+  if (isValidSquare grid nextPosn) && (isNothing $ Map.lookup nextPosn entities) then
     -- next position is reachable
     do
       bfs' <- bfs
       alreadyReached <- readBfs bfs' xs nextPosn
       case alreadyReached of
-        Just _ -> dijkstra' grid source bfs posns
+        Just _ -> dijkstra' grid entities source bfs posns
         Nothing -> do
           -- We have found a new reachable position
           let nextDist = dist + 1
@@ -122,15 +155,14 @@ dijkstra' grid@(_, xs, _) source bfs ((nextPosn,dist):<|posns) =
                 |> (left nextPosn, nextDist)
                 |> (right nextPosn, nextDist)
           () <- setBfs bfs' xs nextPosn dist
-          dijkstra' grid source (pure bfs') posns'
+          dijkstra' grid entities source (pure bfs') posns'
   else
-    dijkstra' grid source bfs posns
-
+    dijkstra' grid entities source bfs posns
 
 main :: IO ()
 main = do
   (grid@(_, xs, _), entities) <- ingest
-  bfs <- dijkstra grid (22, 22)
-  putStrLn $ showGrid grid
+  bfs <- dijkstra grid entities (22, 22)
+  putStrLn $ showGrid grid entities
   putStrLn $ showDijkstra bfs xs
   pure ()
